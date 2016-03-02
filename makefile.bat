3e67b083c0169c25b3a8f143ec8a97b17a8ab92d
陈东 <chendong@51cto.com>
2016-03-02 17:11:51 +0800

    blog

--- makefile.bat
+++ makefile.bat
@@ -2,9 +2,9 @@ git checkout blog
 git add -A
 git commit -m "blog"
 rm -r ../tmp/*
-cp -r _site/ ../tmp/
+cp -r _site/* ../tmp/
 git checkout master
 rm -r ./*
-cp -r ../tmp/_site/* ./
+cp -r ../tmp/* ./
 git add -A
 git commit -m "master"
